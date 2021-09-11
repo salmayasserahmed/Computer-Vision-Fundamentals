fb311c9b5213834a130956b92abac8c593ff150d
Salma Yasser <70496209+salmayasserahmed@users.noreply.github.com>
2021-09-12 00:45:26 +0200

    Update CannyEdgeDetection.py

--- Using OpenCV/CannyEdgeDetection.py
+++ Using OpenCV/CannyEdgeDetection.py
@@ -1,1 +1,24 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+#To use canny edge detection
+import cv2
+#1. Read The Image
+image = mpimg.imread("TobeCanny.jpg")
 
+#2. Convert into greyscale
+gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY) #Gray Conversion
+plt.imshow(gray, cmap='gray') #Cmap controls the colour map used to display the value when and image have only (M,N)
+
+
+#4. Define Kernel Size for Gaussian smoothing or blurring [Optional Step]
+kernal_size = 5 #Must be an Odd Number
+blur_gray = cv2.GaussianBlur(gray,(kernal_size,kernal_size),0)
+
+#5. Define low and high threshold to run Canny
+low_threshold = 50
+high_threshold = 300
+
+#6. Apply Canny
+edge = cv2.Canny(gray,low_threshold, high_threshold)
+plt.imshow(edge, cmap='Greys_r')
+plt.show()
